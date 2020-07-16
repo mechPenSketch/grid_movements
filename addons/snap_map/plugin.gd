98a4d99ff120bf1f276bbbe57be9bc63047328e6
Lyndon Tan <pikadon92@gmail.com>
2020-07-16 23:50:52 +0800

    Found Snap Settings
    Found a code that gets snap configuration.

--- addons/snap_map/plugin.gd
+++ addons/snap_map/plugin.gd
@@ -3,9 +3,17 @@ extends EditorPlugin
 
 # TO DO
 #	GET UPPER TOOLBAR
-#		ADDING: add_control_to_container()
+#		ADDING: add_control_to_container() / get_editor_interface ()
 #		ENUM: CONTAINER_CANVAS_EDITOR_MENU
+#	FIND CanvasItemEditor
+#		get_editor_interface().get_base_control() > Find snap?
 #	GET SNAP PROPERTIES
+
+# CANVAS SNAP SETTINGS
+var snap_spinbox
+
+var snap_offset
+var snap_step
 
 func _enter_tree():
 	# Initialization of the plugin goes here
@@ -14,6 +22,9 @@ func _enter_tree():
 	#eg: add_custom_type("My Button", "Button", preload("my_button.gd"), preload("icon.png"))
 	#		SIGNALS
 	connect("scene_changed", self, "_on_scene_changed")
+	
+	#		DEFINE SNAP SETTINGS
+	update_snap_settings()
 	pass
 
 
@@ -30,10 +41,40 @@ func _on_scene_changed(scene_root:Node):
 	pass
 
 func edit(TileMap):
-	print("nd")
+	pass
 
 func handles(TileMap):
 	return true
 
 func get_plugin_name():
 	return "Snap Map"
+
+func recursive_get_children(node):
+	var children = node.get_children()
+	if children.size() == 0:
+		return []
+	else:
+		for child in children:
+			children += recursive_get_children(child)
+		return children
+
+func find_snap_controls():
+	var base_control = get_editor_interface().get_base_control()
+	var children = recursive_get_children(base_control)
+
+	#var snap_dialog = children[1728]
+	var snap_dialog
+	for i in children.size():
+		var child = children[i]
+		if child.get_class() == "SnapDialog":
+			snap_dialog = child
+	
+	snap_spinbox = []
+	for child in recursive_get_children(snap_dialog):
+		if child.get_class() == "SpinBox":
+			snap_spinbox.append(child)
+
+func update_snap_settings():
+	find_snap_controls()
+	snap_offset = Vector2(snap_spinbox[0].get_value(), snap_spinbox[1].get_value())
+	snap_step = Vector2(snap_spinbox[2].get_value(), snap_spinbox[3].get_value())
