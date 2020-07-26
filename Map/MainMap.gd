44abd046b79834e9bb81f123bcd7586953577f3a
Lyndon Tan <pikadon92@gmail.com>
2020-07-26 22:40:26 +0800

    Plugin-Node interraction
    Resorting methods; set plugin to interact with SnappableTiles.

--- Map/MainMap.gd
+++ Map/MainMap.gd
@@ -1,16 +1,23 @@
+tool
 extends TileMap
-#tool
 
-func _ready():
-	pass # Replace with function body.
-"""
-func set_cell_size(v2):
-	if Engine.editor_hint:
-		# CONFIGURE SNAP > GRID STEP
-		#	SET TO CELL_SIZE: VECTOR 2
-		CanvasItemEditor.snap_step = v2
-	
-	# SET CELL SIZE
-	cell_size = v2
-	pass
-"""
+class_name SnapboundTiles
+
+enum AspectRatio {NONE, SQUARE, KEEP}
+export(int, 64) var cell_width setget set_cell_width
+export(AspectRatio) var aspect_ratio setget set_aspect_ratio
+export(int, 64) var cell_height setget set_cell_height
+
+signal param_changed
+
+func get_class():
+	return "SnapboundTiles"
+
+func set_cell_width(w):
+	emit_signal("param_changed", "cell_width", w)
+
+func set_aspect_ratio(e):
+	emit_signal("param_changed", "aspect_ratio", e)
+
+func set_cell_height(h):
+	emit_signal("param_changed", "cell_height", h)
--- addons/snap_map/plugin.gd
+++ addons/snap_map/plugin.gd
@@ -1,30 +1,25 @@
 tool
 extends EditorPlugin
 
-# TO DO
-#	GET UPPER TOOLBAR
-#		ADDING: add_control_to_container() / get_editor_interface ()
-#		ENUM: CONTAINER_CANVAS_EDITOR_MENU
-#	FIND CanvasItemEditor
-#		get_editor_interface().get_base_control() > Find snap?
-#	GET SNAP PROPERTIES
+var affected_classes = ["SnapboundTiles"]
+var current_node = null
 
 # CANVAS SNAP SETTINGS
 var snap_spinbox
-
-var snap_offset
-var snap_step
+var snap_offset_x
+var snap_offset_y
+var snap_step_x
+var snap_step_y
+var snap_ratio
 
 func _enter_tree():
 	# Initialization of the plugin goes here
 	
 	# 	Add the new type with a name, a parent type, a script and an icon
 	#eg: add_custom_type("My Button", "Button", preload("my_button.gd"), preload("icon.png"))
-	#		SIGNALS
-	connect("scene_changed", self, "_on_scene_changed")
 	
 	#		DEFINE SNAP SETTINGS
-	update_snap_settings()
+	set_snap_settings()
 	pass
 
 
@@ -34,35 +29,41 @@ func _exit_tree():
 	#	Always remember to remove it from the engine when deactivated
 	#eg: remove_custom_type("My Button")
 	#		SIGNALS
-	disconnect("scene_changed", self, "_on_scene_changed")
 	pass
 
-func _on_scene_changed(scene_root:Node):
-	pass
+func _on_param_changed(param, val):
+	match param:
+		"aspect_ratio":
+			snap_ratio = val
+		"cell_width", "cell_height":
+			var prev_val = get_snap_step_l(param)
+			set_snap_step_l(param, val)
+			
+			# AESPECT RATIO FACTOR
+			if snap_ratio:
+				var other_param = "cell_height" if param == "cell_width" else "cell_width"
+				var other_val = val
+				match snap_ratio:
+					1:
+						other_val = val
+					2:
+						other_val = current_node.get(other_param) * val / prev_val
+				set_snap_step_l(other_param, other_val)
+				set_nodes_params(get_tree().get_edited_scene_root(), other_param, other_val)
+				
+	# MASS SETTING PARAM TO ALL AFFECT NODES
+	set_nodes_params(get_tree().get_edited_scene_root(), param, val)
 
-func edit(TileMap):
-	pass
-
-func handles(TileMap):
-	return true
-
-func get_plugin_name():
-	return "Snap Map"
-
-func recursive_get_children(node):
-	var children = node.get_children()
-	if children.size() == 0:
-		return []
-	else:
-		for child in children:
-			children += recursive_get_children(child)
-		return children
+# IF THIS PLUGIN handles(the_selected_node),
+func edit(node):
+	# CONNECT SELECTED NODE
+	node.connect("param_changed", self, "_on_param_changed")
+	snap_ratio = node.aspect_ratio
 
 func find_snap_controls():
 	var base_control = get_editor_interface().get_base_control()
 	var children = recursive_get_children(base_control)
 
-	#var snap_dialog = children[1728]
 	var snap_dialog
 	for i in children.size():
 		var child = children[i]
@@ -74,7 +75,53 @@ func find_snap_controls():
 		if child.get_class() == "SpinBox":
 			snap_spinbox.append(child)
 
-func update_snap_settings():
+func get_plugin_name():
+	return "Snap Map"
+			
+func get_snap_step_l(param):
+	match param:
+		"cell_width":
+			return snap_step_x
+		"cell_height":
+			return snap_step_y
+
+# UPON A NODE BEING SELECTED,
+#	 CHECKS WHETHER IT SHOULD BE AFFECTED BY THIS PLUGIN
+func handles(node):
+	if current_node != null:
+		current_node.disconnect("param_changed", self, "_on_param_changed")
+		current_node = null
+	return affected_classes.has(node.get_class())
+
+func recursive_get_children(node):
+	var children = node.get_children()
+	if children.size() == 0:
+		return []
+	else:
+		for child in children:
+			children += recursive_get_children(child)
+		return children
+
+func set_nodes_params(node, param, val):
+	if affected_classes.has(node.get_class()):
+		node.set(param, val)
+		
+	if node.get_child_count():
+		for c in node.get_children():
+			set_nodes_params(c, param, val)
+
+func set_snap_step_l(param, val):
+	match param:
+		"cell_width":
+			snap_step_x = val
+			snap_spinbox[2].set_value(val)
+		"cell_height":
+			snap_step_y = val
+			snap_spinbox[3].set_value(val)
+
+func set_snap_settings():
 	find_snap_controls()
-	snap_offset = Vector2(snap_spinbox[0].get_value(), snap_spinbox[1].get_value())
-	snap_step = Vector2(snap_spinbox[2].get_value(), snap_spinbox[3].get_value())
+	snap_offset_x = snap_spinbox[0].get_value()
+	snap_offset_y = snap_spinbox[1].get_value()
+	snap_step_x = snap_spinbox[2].get_value()
+	snap_step_y = snap_spinbox[3].get_value()
