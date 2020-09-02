1aa2d5bd5da9f50db1cec88a4d7f6e2bc4316c91
Lyndon Tan <pikadon92@gmail.com>
2020-09-02 22:06:18 +0800

    PlaingPiece Budding
    Created a new class that inherits PlayingPiece. I allows moving via
    player's input.

--- addons/snap_map/nodes/playing_piece.gd
+++ addons/snap_map/nodes/playing_piece.gd
@@ -12,67 +12,8 @@ export(int) var grid_x setget set_grid_x
 export(int) var grid_y setget set_grid_y
 var input_event
 
-# INDIVIDUAL PARAMETERS
-var direction = Vector2()
-const DEG_UP = 0
-const DEG_RIGHT = 90
-const DEG_DOWN = 180
-const DEG_LEFT = 270
-
-var grid
-
-var is_moving = false
-var tween
-var target_pos = Vector2()
-var blocks = []
-var is_blocked:bool = false
-export (Dictionary) var raycast_directions
-var raycast
-export (Resource) var incoming
-signal incoming_gone
-
 # SETTING PARAM CHANGES
 signal param_changed
-
-func _ready():
-	if !Engine.editor_hint:
-		set_notify_transform(true)
-		
-		grid = get_parent()
-	
-		tween = $Tween
-		tween.connect_into(self)
-		turn(Vector2(0,1))
-	pass
-
-func _input(event):
-	if !Engine.editor_hint:
-		direction = Vector2()
-			
-		if event.is_action_pressed("ui_up"):
-			direction.y -= 1
-		elif event.is_action_pressed("ui_down"):
-			direction.y += 1
-		elif event.is_action_pressed("ui_left"):
-			direction.x -= 1
-		elif event.is_action_pressed("ui_right"):
-			direction.x += 1
-		
-		if !is_moving and direction != Vector2():
-			
-			turn(direction)
-			
-			if !raycast.is_colliding():
-				target_pos = get_position() + direction * grid.get_cell_size()
-				
-				# ADD INCOMING BLOCK
-				var new_incoming = incoming.instance()
-				new_incoming.set_position(target_pos)
-				grid.add_child(new_incoming)
-				connect("incoming_gone", new_incoming, "queue_free")
-				
-				tween.move_char(self, target_pos)
-				is_moving = true
 
 func _notification(what):
 	if what == NOTIFICATION_TRANSFORM_CHANGED:
@@ -81,22 +22,6 @@ func _notification(what):
 				# SET GRID POSITION
 				grid_x = int(position.x) / cell_width
 				grid_y = int(position.y) / cell_width
-
-func _on_tween_completed(o, k):
-	is_moving = false
-	emit_signal("incoming_gone")
-	pass
-
-func _on_area_entered(a):
-	if a.get_parent() != $Position2D:
-		blocks.append(a)
-		is_blocked = true
-	pass
-
-func _on_area_exited(a):
-	blocks.erase(a)
-	is_blocked = blocks.size()
-	pass
 
 func _plugin_input(e):
 	input_event = e
@@ -141,40 +66,8 @@ func set_grid_y(gy):
 	grid_y = gy
 	set_pos_y()
 
-# TO BE OVERRIDDEN TO ACCOUNT FOR GRID POSITIONS
-func set_global_position(value):
-	print(value)
-	"""
-	# ROUNDING
-	var cell_lengths = [cell_width, cell_height]
-	for i in v2.size():
-		# GET THE LENGTH TO BE USED
-		var l = cell_lengths[i]
-		
-		# GET REMAINDER
-		var r = v2[i] % l
-		
-		# GET MID-POINT
-		var m = l / 2
-		if r < m:
-			# ROUND TO PREVIOUS MID-POINT
-			var diff = m - r
-			cell_lengths[i] += diff
-		elif r > m:
-			# ROUND TO NEXT MID-POINT
-			var diff = r - m
-			cell_lengths[i] -= diff
-	print(v2)
-	"""
-	grid_x = int(position.x) / cell_width
-	grid_y = int(position.y) / cell_width
-	.set_global_position(value)
-
 func set_pos_x():
 	position.x = (grid_x + 0.5) * cell_width
 	
 func set_pos_y():
 	position.y = (grid_y + 0.5) * cell_height
-
-func turn(dir:Vector2):
-	raycast = get_node(raycast_directions[dir])
