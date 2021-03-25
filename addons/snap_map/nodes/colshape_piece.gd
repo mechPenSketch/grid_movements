f2db87a6629b65765e8c3c15663a7d2eb39663aa
Lyndon Tan <pikadon92@gmail.com>
2021-03-26 00:41:49 +0800

    Ripped out Parameters

--- addons/snap_map/nodes/colshape_piece.gd
+++ addons/snap_map/nodes/colshape_piece.gd
@@ -3,12 +3,6 @@ extends CollisionShape2D
 
 class_name ColShapePiece, "colshape_piece.svg"
 
-# SNAP SETTINGS
-enum AspectRatio {NONE, SQUARE, KEEP}
-export(int) var cell_width = 64 setget set_cell_width
-export(AspectRatio) var aspect_ratio = AspectRatio.SQUARE setget set_aspect_ratio
-export(int) var cell_height = 64 setget set_cell_height
-
 # SETTING PARAM CHANGES
 signal param_changed
 
@@ -19,95 +13,3 @@ func get_class():
 
 func is_class(s)->bool:
 	return s == get_class() or .is_class(s)
-
-# SHAPE DATA
-
-func _enter_tree():
-	if shape == null:
-		var new_shape = create_new_shape()
-		shape = new_shape
-		set_shape_width()
-		set_shape_height()
-
-func create_new_shape():
-	return load(get_script().get_path().get_base_dir() + "/colshape.tres")
-
-func get_half_width():
-	match shape.get_class():
-		"RectangleShape2D":
-			return shape.extents.x
-		"CircleShape2D", "CapsuleShape2D":
-			return shape.radius
-		_:
-			return shape.width
-
-func get_half_height():
-	match shape.get_class():
-		"RectangleShape2D":
-			return shape.extents.y
-		"CircleShape2D":
-			return shape.radius
-		_:
-			return shape.height
-
-# SETTING PROPERTIES THROUGH PLUGIN
-
-func plugset_cell_width(w):
-	cell_width = w
-	set_shape_width()
-
-func plugset_aspect_ratio(e):
-	aspect_ratio = e
-	
-func plugset_cell_height(h):
-	cell_height = h
-	set_shape_height()
-
-# SETTING PROPERTIES THROUGH INSPECTOR
-
-func set_cell_width(w):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_width", w, self)
-	else:
-		cell_width = w
-
-func set_aspect_ratio(e):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "aspect_ratio", e)
-	else:
-		aspect_ratio = e
-
-func set_cell_height(h):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_height", h, self)
-	else:
-		cell_height = h
-	
-# SETTING SHAPE DIMENSIONS
-
-func set_shape(val):
-	.set_shape(val)
-	set_shape_width()
-	set_shape_height()
-	val.property_list_changed_notify()
-
-func set_shape_width():
-	match shape.get_class():
-		"RectangleShape2D":
-			set_rect_width()
-		"CircleShape2D", "CapsuleShape2D":
-			set_circle_r()
-			
-func set_shape_height():
-	match shape.get_class():
-		"RectangleShape2D", "CapsuleShape2D":
-			set_rect_height()
-
-func set_rect_width():
-	shape.extents.x = cell_width/4
-
-func set_rect_height():
-	shape.extents.y = cell_height/4
-	
-func set_circle_r():
-	shape.radius = cell_width/4
--- addons/snap_map/nodes/raycast_piece.gd
+++ addons/snap_map/nodes/raycast_piece.gd
@@ -3,12 +3,6 @@ extends RayCast2D
 
 class_name RayCastPiece, "raycast_piece.svg"
 
-# SNAP SETTINGS
-enum AspectRatio {NONE, SQUARE, KEEP}
-export(int) var cell_width = 64 setget set_cell_width
-export(AspectRatio) var aspect_ratio = AspectRatio.SQUARE setget set_aspect_ratio
-export(int) var cell_height = 64 setget set_cell_height
-
 # INDIVIDUAL PARAMETERS
 export(Vector2) var direction_ratio setget set_direction_ratio
 
@@ -23,43 +17,10 @@ func get_class():
 func is_class(s)->bool:
 	return s == get_class() or .is_class(s)
 
-# SETTING PROPERTIES THROUGH PLUGIN
-
-func plugset_cell_width(w):
-	cell_width = w
-	var ratio = direction_ratio.x
-	if ratio > 0:
-		position.x = w / 2
-		cast_to.x = w * (ratio - 0.5)
-	elif ratio < 0:
-		position.x = -w / 2
-		cast_to.x = w * (ratio + 0.5)
-	else:
-		position.x = 0
-		cast_to.x = 0
-
-func plugset_aspect_ratio(e):
-	aspect_ratio = e
-	
-func plugset_cell_height(h):
-	cell_height = h
-	var ratio = direction_ratio.y
-	if ratio > 0:
-		position.y = h / 2
-		cast_to.y = h * (ratio - 0.5)
-	elif ratio < 0:
-		position.y = -h / 2
-		cast_to.y = h * (ratio + 0.5)
-	else:
-		position.y = 0
-		cast_to.y = 0
-
-func get_snap_step()->Vector2:
-	return Vector2(cell_width, cell_height)
-
 # SETTING PROPERTIES THROUGH INSPECTOR
 
 func set_direction_ratio(v2):
+	"""
 	var prev_dr = direction_ratio
 	direction_ratio = v2
 	
@@ -70,21 +31,5 @@ func set_direction_ratio(v2):
 		plugset_cell_height(cell_height)
 		
 	property_list_changed_notify()
-
-func set_cell_width(w):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_width", w, self)
-	else:
-		cell_width = w
-
-func set_aspect_ratio(e):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "aspect_ratio", e)
-	else:
-		aspect_ratio = e
-
-func set_cell_height(h):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_height", h, self)
-	else:
-		cell_height = h
+	"""
+	pass
