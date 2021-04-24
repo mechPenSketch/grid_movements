67b5f1d02e167e5b79500202318d26f92e24657d
mechPenSketch <pikadon92@gmail.com>
2021-04-24 19:23:28 +0800

    Remove parameters from SnapboundTiles

--- addons/snap_map/nodes/snapbound_tiles.gd
+++ addons/snap_map/nodes/snapbound_tiles.gd
@@ -3,11 +3,6 @@ extends TileMap
 
 class_name SnapboundTiles, "snapbound_tiles.svg"
 
-enum AspectRatio {NONE, SQUARE, KEEP}
-export(int) var cell_width = 64 setget set_cell_width
-export(AspectRatio) var aspect_ratio = AspectRatio.SQUARE setget set_aspect_ratio
-export(int) var cell_height = 64 setget set_cell_height
-
 # SETTING PARAM CHANGES
 signal param_changed
 
@@ -18,36 +13,3 @@ func get_class():
 
 func is_class(s)->bool:
 	return s == get_class() or .is_class(s)
-
-# SETTING PROPERTIES THROUGH PLUGIN
-
-func plugset_cell_width(w):
-	cell_width = w
-	cell_size.x = w
-
-func plugset_aspect_ratio(e):
-	aspect_ratio = e
-	
-func plugset_cell_height(h):
-	cell_height = h
-	cell_size.y = h
-
-# SETTING PROPERTIES THROUGH INSPECTOR
-
-func set_cell_width(w):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_width", w, self)
-	else:
-		cell_width = w
-
-func set_aspect_ratio(e):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "aspect_ratio", e)
-	else:
-		aspect_ratio = e
-
-func set_cell_height(h):
-	if Engine.editor_hint:
-		emit_signal("param_changed", "cell_height", h, self)
-	else:
-		cell_height = h
