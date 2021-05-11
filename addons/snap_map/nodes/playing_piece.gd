0ce65ec7b7c10f11a573b62ac0950b75ecadd7e9
mechPenSketch <pikadon92@gmail.com>
2021-05-11 11:57:15 +0800

    A Bit More Pruning

--- addons/snap_map/nodes/playing_piece.gd
+++ addons/snap_map/nodes/playing_piece.gd
@@ -3,16 +3,7 @@ extends Area2D
 
 class_name PlayingPiece, "playing_piece.svg"
 
-var input_event
-
-# SETTING PARAM CHANGES
-signal param_changed
-
-func _notification(what):
-	if what == NOTIFICATION_TRANSFORM_CHANGED:
-		if input_event is InputEventMouseButton:
-			if !input_event.is_pressed():
-				pass
+var grid_position = null
 
 # CLASS DATA
 
